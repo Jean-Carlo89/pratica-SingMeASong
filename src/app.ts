566ab193ef7f7a225866e316ddbd0c628030d687
Jean Carlo <jeancarlomo@hotmail.com>
2021-07-22 16:03:58 -0300

    criaçao das pastas de arquitetura e começo d rota post recomedatitons

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 
+import * as recomendationsController from './controllers/recomendationsController'
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -8,5 +9,7 @@ app.use(express.json());
 app.get("/test", (req, res) => {
   res.send("OK!");
 });
+
+app.post("/recomendations", recomendationsController.addRecomendation)
 
 export default app;
